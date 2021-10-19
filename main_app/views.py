0cc05c620c7627b6ac6a97e343560c027dd889d9
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-18 20:58:54 -0600

    added home view function in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
+from django.http.request import HttpRequest
 from django.shortcuts import render
 
 # add the following import 
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+# defined the home view 
+def home(request):
+    return HttpRequest('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
