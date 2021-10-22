35e76e307b47ae1e84d7bbee027fe9eafb827452
AustinLeaming <austin@Austins-Macbook-Air.local>
2021-10-21 21:46:09 -0600

    created path for accessories in urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('guitars/<int:pk>/update/', views.GuitarUpdate.as_view(), name='guitars_update'),
     path('guitars/<int:pk>/delete/', views.GuitarDelete.as_view(), name='guitars_delete'),
     path('guitars/<int:guitar_id>/add_tuning', views.add_tuning, name='add_tuning'),
+    path('accessories/', views.AccessoryList.as_view(), name='accessories_index'),
+    path('accessories/<int:pk>/', views.AccessoryDetail.as_view(), name='accessories_detail'),
+    path('accessories/create/', views.AccessoryCreate.as_view(), name='accessories_create'),
+    path('accessories/<int:pk>/update/', views.AccessoryUpdate.as_view(), name='accessories_update'),
+    path('accessories/<int:pk>/delete/', views.AccessoryDelete.as_view(), name='accessories_delete'),
 ]
